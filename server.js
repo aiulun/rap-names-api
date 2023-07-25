2c725003bbd4884f7e194007798ac54cca6e326e
Alan Liang <aiu.lun@gmail.com>
2023-07-25 12:25:15 -0700

    added heroku port

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.get('/api/:name', (request, response) => {
 })
 
 const PORT = 8000;
-app.listen(PORT, ()=> {
+app.listen(process.env.PORT || PORT, ()=> {
     console.log(`Server is running on port ${PORT}`)
 })
